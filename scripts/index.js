be10c0080bbf6b284815386880f94122dffc6517
Lo Hill <hillkaitlin01@gmail.com>
2024-03-11 23:58:57 -0600

    Add modal pop-up

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,17 @@ let initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector('#profile-edit-button');
+const profileEditModal = document.querySelector('#profile-edit-modal');
+const profileEditClose = document.querySelector('#profile-edit-close');
+
+profileEditButton.addEventListener('click', () => {
+    profileEditModal.classList.add('modal_opened');
+})
+
+profileEditClose.addEventListener('click', () => {
+    profileEditModal.classList.remove('modal_opened');
+})
+
+
